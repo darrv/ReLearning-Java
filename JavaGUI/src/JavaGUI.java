2c1f335423eab442a2580fdd7b98f65a81a58981
darrv <143595561+darrv@users.noreply.github.com>
2023-10-21 01:28:15 +0800

    Add files via upload

---
+++ JavaGUI/src/JavaGUI.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane; // import class
+
+public class JavaGUI {
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("Please tell me your name"); // get user input
+		JOptionPane.showMessageDialog(null, "Welcome " +name);
+		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Please tell me your age"));  // int conversion
+		JOptionPane.showMessageDialog(null, "Your age is " +age);
+		
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Please tell me your height(cm)"));  // double conversion
+		JOptionPane.showMessageDialog(null, "Your height is " +height+ "cm tall");
+		
+	}
+}
