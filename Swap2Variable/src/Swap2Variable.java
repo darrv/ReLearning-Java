08029a5614929e61dbad1c230c24aadc5c8c9a58
darrv <143595561+darrv@users.noreply.github.com>
2023-10-21 00:58:47 +0800

    Add files via upload

---
+++ Swap2Variable/src/Swap2Variable.java
@@ -0,0 +1,18 @@
+
+public class Swap2Variable { // Swapping 2 variables by "Bro Code" from youtube
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x = "water";
+		String y = "Kool-Aid";
+		String temp;
+		
+		temp = x; // water
+		x = y; // x = Kool-Aid
+		y = temp; // y = water
+		
+		System.out.println("x: " + x);
+		System.out.println("y: " + y);
+	}
+
+}
