c381dd23f5a389bdb7cdb56bffb6d458b608c6af
darrv <143595561+darrv@users.noreply.github.com>
2023-10-24 18:30:50 +0800

    Add files via upload

---
+++ StringMethods/src/StringMethods.java
@@ -0,0 +1,24 @@
+/* Learning Java String methods from "Bro Code" in youtube */
+
+
+public class StringMethods {
+	public static void main(String[] args) {
+		String name = "Bro";
+		
+		//boolean result = name.equalsIgnoreCase("bro");	
+		//int result = name.length();
+		//char result = name.charAt(2);		
+		//int result = name.indexOf("o");		
+		//boolean result = name.isEmpty();
+		
+		//String result = name.toUpperCase();
+		//String result = name.toLowerCase();
+		//String result = name.trim();
+		String result = name.replace('o', 'B');
+		
+	
+		
+		
+		System.out.println(result);
+	}
+}
