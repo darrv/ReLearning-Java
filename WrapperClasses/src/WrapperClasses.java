b2bd5c38a1c767badf770215b510f48ab24f107e
darrv <143595561+darrv@users.noreply.github.com>
2023-10-24 18:39:07 +0800

    Add files via upload

---
+++ WrapperClasses/src/WrapperClasses.java
@@ -0,0 +1,19 @@
+/* Learning Wrapper Classes from "Bro Code" in youtube */
+
+
+public class WrapperClasses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Boolean a = true;
+			Character b = '$';
+			Integer c = 321;
+			Double d = 7.89;
+			String e = "Darren";
+			
+			if (b=='@') {
+				System.out.println("This is true");
+			}
+	}
+
+}
