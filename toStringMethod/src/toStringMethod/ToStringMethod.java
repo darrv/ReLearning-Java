d677333849ccb493711de1a5a72e47d74bd5b93c
darrv <143595561+darrv@users.noreply.github.com>
2023-10-28 18:18:10 +0800

    Add files via upload

---
+++ toStringMethod/src/toStringMethod/ToStringMethod.java
@@ -0,0 +1,20 @@
+/* Learning toString Method from "Bro Code" Youtube */
+
+package toStringMethod;
+
+public class ToStringMethod {
+	public static void main(String[] args) {
+		
+		Car car1 = new Car();
+		
+//		System.out.println(car1.make);
+//		System.out.println(car1.model);
+//		System.out.println(car1.color);
+//		System.out.println(car1.year);
+		
+		
+		
+//		System.out.println(car1.toString());
+		System.out.println(car1);
+	}
+}
