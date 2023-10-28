71a55dfa66921ffe1f1fd91606e76005cfed4d0e
darrv <143595561+darrv@users.noreply.github.com>
2023-10-28 17:21:44 +0800

    Add files via upload

---
+++ FinalKeyword/src/FinalKeyword.java
@@ -0,0 +1,14 @@
+/* Learning final keyword from "Bro Code" Youtube */
+
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.14159; // standard practice to caps variable name
+		
+		//pi = 4;
+		
+		System.out.println(PI);
+	}
+
+}
