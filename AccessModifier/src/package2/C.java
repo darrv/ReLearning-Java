58360dbba3e210b07e9f7593f5125f782cad483b
darrv <143595561+darrv@users.noreply.github.com>
2023-11-02 19:41:37 +0800

    Update C.java

--- AccessModifier/src/package2/C.java
+++ AccessModifier/src/package2/C.java
@@ -3,12 +3,15 @@ import package1.*;
 
 public class C {
 	
+// public : visible to any packages within project folder	
 	public String publicMessage = "Public message"; 
 	
- // protected: accessible to different class/package as long as that class is a child class of this class(C)	
+// protected: accessible to different class/package as long as that class is a child class of this class(C)	
 	protected String protectedMessage = "Protected Message";
+
+// by default, only works in same package
+	          String defaultMessage = "This is the default";
 	
-	String defaultMessage = "This is the default";
 // only visible to its own class
 	private String privateMessage = "Private Message";
 
